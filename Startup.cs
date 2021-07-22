8cea7650b3030a7abceabeed5dd5d2d5af109eb0
Guilherme Bandeira <band.92@hotmail.com>
2021-07-22 10:07:35 -0300

    First commit

---
+++ Startup.cs
@@ -0,0 +1,59 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace ConfigSwagger
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("V1", new Info { Title = "Empresa X", Version = "V1", Contact = new OpenApiContact { Name = "Gustavo Braga", Email = "Gustavo@gustavobraga.com.br", Url=new Uri("teste@teste")}}) ;
+                            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.RoutePrefix = "Swagger";
+                c.SwaggerEndpoint("/Swagger/V1/swagger.json", "API Guilherme");
+            });
+        }
+    }
+}
